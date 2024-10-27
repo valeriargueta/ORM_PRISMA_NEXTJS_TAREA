9297f441445da6917d398a2b2761b43a0675261b
Valeria Argueta - PMO Manager <vargueta@develsystems.com>
2024-10-27 17:30:17 -0600

    subi tarea

---
+++ prisma/migrations/20241027222906_init/migration.sql
@@ -0,0 +1,29 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- CreateTable
+CREATE TABLE [dbo].[Product] (
+    [id] INT NOT NULL IDENTITY(1,1),
+    [code] NVARCHAR(1000) NOT NULL,
+    [name] NVARCHAR(1000) NOT NULL,
+    [description] NVARCHAR(1000) NOT NULL,
+    [price] FLOAT(53) NOT NULL,
+    [createdAt] DATETIME2 NOT NULL CONSTRAINT [Product_createdAt_df] DEFAULT CURRENT_TIMESTAMP,
+    [updatedAt] DATETIME2 NOT NULL,
+    CONSTRAINT [Product_pkey] PRIMARY KEY CLUSTERED ([id]),
+    CONSTRAINT [Product_code_key] UNIQUE NONCLUSTERED ([code])
+);
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
